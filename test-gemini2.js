3c73ec236205d2a3619cee5fc62b3dea70960e48
mroya <royamarcio@gmail.com>
2026-05-10 17:39:19 -0300

    Fix api 10.4

--- test-gemini2.js
+++
@@ -1,6 +0,0 @@
-async function run() {
-  const res = await fetch('https://generativelanguage.googleapis.com/v1beta/models?key=' + 'AIzaSyDNXq8aicE0_yTRfXEuEHQfq4-bkVQLONk');
-  const data = await res.json();
-  console.log(JSON.stringify(data.models.map(m => m.name), null, 2));
-}
-run();
